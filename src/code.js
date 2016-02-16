ee0c3afc6faca911645cad22883d6fddf7043f55
Andrew (Andy) Clemens <wikipedianerd@gmail.com>
2016-02-16 00:24:06 -0600

    Update code.js

--- src/code.js
+++ src/code.js
@@ -589,7 +589,7 @@ function updateURL() {
   var code = getCode();
   var url = window.location.href.split('?')[0] + code;
   history.pushState('data', '', url);
-  _gaq.push(['_setCustomVar',1,'URL',window.location.href]);
+  ga('set', 'URL', code);
 }
 
 function getCode() {
