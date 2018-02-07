fd6e929328fffd66b26fe352650dfc248009ede2
Andrew Clemens <wikipedianerd@gmail.com>
2018-02-07 13:54:05 -0500

    Update code.js

--- dev/code.js
+++ dev/code.js
@@ -584,6 +584,7 @@ $('.qs_title_bar').on('click tap', function() {settings.toggleCollapsed()});
         updateURL();
     });
     cy.on('tapend', function() {
+        if (typeof evt.cyTarget.length) == 'undefined') {undisplayMovie();}
         updateURL();
     });
     document.oncontextmenu = function() {
